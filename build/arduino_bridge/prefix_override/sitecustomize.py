62dc61089d01478a6af23bd5b8f236c00fd43f00
Murilo Passarelli <murilo.brenneken.passarelli@gmail.com>
2025-05-14 09:25:00 -0300

    Add camera packages

--- build/arduino_bridge/prefix_override/sitecustomize.py
+++ build/arduino_bridge/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/murilo/robo_iris_ws/install/arduino_bridge'
+    sys.prefix = sys.exec_prefix = '/home/robofei/RoboIris/install/arduino_bridge'
---
+++ build/face_detector/build/lib/face_detector/face_detector_node.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class FaceDetector(Node):
+    def __init__(self):
+        super().__init__('face_detector')
+        self.subscription = self.create_subscription(
+            Image,
+            '/camera/image_raw',
+            self.listener_callback,
+            10)
+        self.bridge = CvBridge()
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.get_logger().info("Face detector node started.")
+
+    def listener_callback(self, msg):
+        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow("Face Detection", frame)
+        cv2.waitKey(1)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = FaceDetector()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
